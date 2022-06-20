c8491755b5f0855074195d88938eb04bba08d5bc
glbreeze <glbreeze@gmail.com>
2022-06-20 22:04:09 +0800

    fix_nan_loss

--- src/train_fuse.py
+++ src/train_fuse.py
@@ -182,6 +182,7 @@ def main(args: argparse.Namespace) -> None:
             # Loss function: Dynamic class weights used for query image only during training
             wt_mask = (pred_q0.argmax(dim=1) != pred_q1.argmax(dim=1)) & (q_label != 255)
             criterion = torch.nn.CrossEntropyLoss(ignore_index=255, reduction='none')
+            wt_mask[wt_mask==False] = 0.001
             q_loss = torch.sum( criterion(pred_q, q_label.long()) * wt_mask ) / torch.sum(wt_mask)   # assume batch_size 1
 
             optimizer_meta.zero_grad()
