a05dcd8a7642b0adfeeed092379a3a02347d4095
glbreeze <glbreeze@gmail.com>
2022-06-20 16:35:14 +0800

    bug_fix

--- src/train_fuse.py
+++ src/train_fuse.py
@@ -36,7 +36,7 @@ def parse_args() -> argparse.Namespace:
 
 def main(args: argparse.Namespace) -> None:
 
-    sv_path = 'fuse_{}/{}{}/shot{}_split{}/{}'.format(
+    sv_path = 'fuse_{}/{}{}/split{}_shot{}/{}'.format(
         args.train_name, args.arch, args.layers, args.train_split, args.shot, args.exp_name)
     sv_path = os.path.join('./results', sv_path)
     ensure_path(sv_path)
