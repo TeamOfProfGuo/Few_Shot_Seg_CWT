54618c781438b6173ab1589d991d625f491d05ea
glbreeze <glbreeze@gmail.com>
2022-06-29 16:08:04 +0800

    l34_dice_cat_nv

--- src/model/mmn.py
+++ src/model/mmn.py
@@ -9,26 +9,21 @@ from .model_util import SegLoss, get_corr
 
 
 class MMN(nn.Module):
-    def __init__(self, args, agg='cat', inner_channel=32, sem=True, wa=False):
+    def __init__(self, args, agg='cat', wa=False, sem=False,):
         super().__init__()
         self.args = args     # rmid
         self.agg = agg
         match_ch = 1 if agg=='sum' else len(args.rmid)-1
+        self.bid_lst = [int(num) - 1 for num in list(args.rmid[1:])]  # [0, 1, 2, 3]
 
-        self.sem = sem
         self.wa = wa
         if self.args.layers == 50:
             self.nbottlenecks = [3, 4, 6, 3]
             self.feature_channels = [256, 512, 1024, 2048]
-
-        self.bids = [int(num) for num in list(args.rmid[1:])]
-        for i, b in enumerate(self.bids):
-            c_in = self.feature_channels[b-1]
-            if self.sem:
-                setattr(self, "msblock"+str(b), MSBlock(c_in, rate=4, c_out=inner_channel))
-                c_in = inner_channel
-            if self.wa:
-                setattr(self, "wa_"+str(b), WeightAverage(c_in))
+        if self.wa:
+            for bid in self.bid_lst:
+                c_in = self.feature_channels[bid]
+                setattr(self, "wa_"+str(bid), WeightAverage(c_in))
 
         self.corr_net = MatchNet(temp=args.temp, cv_type='red', sce=False, cyc=False, sym_mode=True, in_channel=match_ch)
 
@@ -36,10 +31,14 @@ class MMN(nn.Module):
         B, ch, h, w = f_q.shape
 
         corr_lst = []
-        idx_lst = [int(num)-1 for num in list(self.args.rmid[1:])]
-        for idx in idx_lst[::-1]:
-            fq_fea = F.normalize(fq_lst[idx], dim=1)
-            fs_fea = F.normalize(fs_lst[idx], dim=1)
+        for idx in self.bid_lst[::-1]:
+            fq_fea = fq_lst[idx]
+            fs_fea = fs_lst[idx]
+            if self.wa:
+                fq_fea = getattr(self, "wa_"+str(idx))(fq_fea)
+                fs_fea = getattr(self, 'wa_'+str(idx))(fs_fea)
+            fq_fea = F.normalize(fq_fea, dim=1)
+            fs_fea = F.normalize(fs_fea, dim=1)
             corr = get_corr(fq_fea, fs_fea)
             corr4d = corr.view(B, -1, h, w, h, w)
             corr_lst.append(corr4d)
@@ -47,6 +46,8 @@ class MMN(nn.Module):
         corr4d = torch.cat(corr_lst, dim=1)   # [B, L, h, w, h, w]
         if self.agg == 'sum':
             corr4d = torch.sum(corr4d, dim=1, keepdim=True)  # [B, 1, h, w, h, w]
+
+        f_s = F.normalize(f_s, dim=1)
 
         att_fq = self.corr_net.corr_forward(corr4d, v=f_s)
         fq = F.normalize(f_q, p=2, dim=1) + F.normalize(att_fq, p=2, dim=1) * self.args.att_wt
