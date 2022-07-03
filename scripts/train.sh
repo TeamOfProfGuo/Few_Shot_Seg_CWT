95b1e6e4411ee8bc160c3b969632f1a8d8851a08
glbreeze <glbreeze@gmail.com>
2022-07-03 16:39:31 +0800

    cwt_coco

--- scripts/train.sh
+++ scripts/train.sh
@@ -25,6 +25,7 @@ mkdir -p -- "$dirname"
 echo "start"
 singularity exec --nv \
             --overlay /scratch/lg154/python36/python36.ext3:ro \
+            --overlay /scratch/lg154/sseg/dataset/coco2014.sqf:ro \
             /scratch/work/public/singularity/cuda11.2.2-cudnn8-devel-ubuntu20.04.sif \
             /bin/bash -c " source /ext3/env.sh;
             python -m src.train --config config_files/${DATA}.yaml \
