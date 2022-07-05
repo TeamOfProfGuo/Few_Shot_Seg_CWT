172e68f5560d1df957c8155759d78d3234404e15
glbreeze <glbreeze@gmail.com>
2022-07-05 15:56:09 +0800

    exp_cv4

--- src/model/mmn.py
+++ src/model/mmn.py
@@ -33,7 +33,7 @@ class MMN(nn.Module):
             match_ch = 1
         else:
             match_ch = sum([self.nbottlenecks[i-1] if str(i) in str(args.all_lr) else 1 for i in self.bid_lst])
-        self.corr_net = MatchNet(temp=args.temp, cv_type='red', sce=False, cyc=False, sym_mode=True, in_channel=match_ch)
+        self.corr_net = MatchNet(temp=args.temp, cv_type= args.get('conv4d', 'red'), sce=False, cyc=False, sym_mode=True, in_channel=match_ch)
 
     def forward(self, fq_lst, fs_lst, f_q, f_s):   # fq_lst: dict{bid: [bottleneck layers]}
         B, ch, h, w = f_q.shape
