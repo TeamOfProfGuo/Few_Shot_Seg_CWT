677d943a3ade58878d0750cdd51d39394884dfd3
glbreeze <glbreeze@gmail.com>
2022-07-15 14:30:48 +0800

    add_coco_dataset

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -22,6 +22,7 @@ SHOT=$4
 echo "start"
 singularity exec --nv \
             --overlay /scratch/lg154/python36/python36.ext3:ro \
+            --overlay /scratch/lg154/sseg/dataset/coco2014.sqf:ro \
             /scratch/work/public/singularity/cuda11.2.2-cudnn8-devel-ubuntu20.04.sif \
             /bin/bash -c " source /ext3/env.sh;
             python -m src.pretrain --config config_files/${DATA}_pretrain.yaml \
